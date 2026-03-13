aaa306754db7d2fb0844c5496b87306727c828ef
sourov_das <sourovsd0009@gmail.com>
2026-03-13 19:43:45 +0600

    Remove 'Skills' navigation link.

--- app/constants/navigation.ts
+++ app/constants/navigation.ts
@@ -1,6 +1,6 @@
 export const navigationLinks = [
   { href: '#hero', label: 'Home' },
   { href: '#about', label: 'About' },
-  { href: '#skills', label: 'Skills' },
+
   { href: '#projects', label: 'Projects' },
 ];
